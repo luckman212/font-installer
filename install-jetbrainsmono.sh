7f00ba7200428103f7ee7d7912e26512b4f8ae10
luckman212 <1992842+luckman212@users.noreply.github.com>
2023-06-18 13:01:07 -0400

    init

---
+++ install-jetbrainsmono.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+_die() {
+  [[ -n $1 ]] && echo "$1"
+  exit ${2:-1}
+}
+
+# satisfy prereqs
+if ! hash brew &>/dev/null; then
+  _die "requires Homebrew, visit https://brew.sh for instructions"
+  exit 1
+fi
+if ! hash otfinfo &>/dev/null; then
+  brew install --quiet lcdf-typetools
+fi
+if ! hash jq &>/dev/null; then
+  brew install --quiet jq
+fi
+
+REPO='JetBrains/JetBrainsMono'
+ODIR="${HOME}/Downloads"
+CURLOPTS=( --location --connect-timeout 15 --max-time 60 )
+LOCAL_VER=$(otfinfo -v ~/Library/Fonts/JetBrainsMono-Regular.ttf 2>/dev/null)
+[[ $LOCAL_VER =~ ([0-9.-]+) ]] && CUR_VER=${BASH_REMATCH[1]}
+
+# get release info from GitHub
+JSON=$(
+  curl "${CURLOPTS[@]}" --silent https://api.github.com/repos/${REPO}/releases |
+  jq 'map(select(.prerelease==false and (.tag_name|contains("beta")|not)))[0]'
+)
+[[ -z $JSON ]] || [[ $JSON == "null" ]] && _die "error fetching data from GitHub"
+LATEST_TAG=$(jq -r .tag_name <<<"$JSON")
+[[ $LATEST_TAG =~ ([0-9.-]+) ]] && LATEST_VER=${BASH_REMATCH[1]}
+[[ -n $LATEST_VER ]] || _die "error parsing latest release tag from GitHub"
+[[ ${LATEST_VER} == "${CUR_VER}" ]] && _die "version ${CUR_VER} already installed" 0
+ZIP_URLS=$(jq -r '.assets[] | select(.name|match("JetBrainsMono.*\\.zip")) | .browser_download_url' <<<"$JSON")
+[[ -n $ZIP_URLS ]] || _die "error obtaining downloadable assets"
+
+# download
+while read -r ZIP_URL; do
+  ZIP_FILENAME=${ZIP_URL##*/}
+  if ! curl "${CURLOPTS[@]}" --progress-bar --remote-name --output-dir "$ODIR" "${ZIP_URL}"; then
+    die "error downloading ${ZIP_FILENAME}"
+  fi
+  unzip -qo "${ODIR}/${ZIP_FILENAME}" 'fonts/ttf/*' -d "${ODIR}/jetbrains"
+  find "${ODIR}/jetbrains/fonts/ttf" -name "*.ttf" -exec cp {} ~/Library/Fonts \;
+  rm "${ODIR}/${ZIP_FILENAME}" 2>/dev/null
+  rm -r "${ODIR}/jetbrains" 2>/dev/null
+done <<<"$ZIP_URLS"
